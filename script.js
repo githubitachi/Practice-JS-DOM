f0f143f0044b2abb84edafb55f3a51d338172988
Mark <markjovendonaire@gmail.com>
2025-07-21 20:34:44 +0800

    Adding html and testing js file

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+// script.js
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
 
 //Form
-
 form.addEventListener('submit', addItem);
 
+
+//addItem
 function addItem(e){
-    e.prevenDefault();
+    e.preventDefault(); // Corrected typo here!
 
     console.log(1);
 }

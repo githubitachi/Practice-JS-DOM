f61b82fa67a1f3712ca3fde1cdffbfe72eb1a988
Mark <markjovendonaire@gmail.com>
2025-07-21 20:46:18 +0800

    Created functions and tested syntax

--- script.js
+++ script.js
@@ -10,5 +10,21 @@ form.addEventListener('submit', addItem);
 function addItem(e){
     e.preventDefault(); // Corrected typo here!
 
-    console.log(1);
+    // Get input value
+    var newItem = document.getElementById('itemInput').value; // Corrected ID here!
+
+    // Create new li element
+    var li = document.createElement('li'); // Correctly create new li element
+
+    // Add class
+    li.className = 'list-group-item';
+
+    // Add text node
+    li.appendChild(document.createTextNode(newItem));
+
+    // Append li to the item list
+    itemList.appendChild(li);
+
+    // Clear the input field after adding the item (optional, but good practice)
+    document.getElementById('itemInput').value = '';
 }
